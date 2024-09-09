ec0213df2a263cf075c0f67914d5098c52e1609d
Solme <174892254+Solmeyt@users.noreply.github.com>
2024-09-09 21:20:46 +0200

    Update RitualBook.java

--- src/main/java/me/ghosty/kamoof/features/ritual/RitualBook.java
+++ src/main/java/me/ghosty/kamoof/features/ritual/RitualBook.java
@@ -43,7 +43,7 @@ public final class RitualBook {
 	
 	private static List<BaseComponent[]> getPages() {
 		List<String> pages = Placeholder.apply(Arrays.asList(
-			"<dark_red>Pacte Ensanglanté<br><br><red>Ce pacte te permet d'augmenter ta vie de <dark_red>5 <red>coeurs t'es proposé, mais ta prochaine mort te coûtera 3 têtes...<br><dark_red><bold>Vas-tu l'accepter ?<br><br><click:run_command:%command1%><red><bold>[Accepter le Pacte]",
+			"<dark_red>Pacte Ensanglanté<br><br><red>Ce pacte te permet d'augmenter ta vie de <dark_red>5 <red>coeurs t'est proposé, mais ta prochaine mort te coûtera 3 têtes...<br><dark_red><bold>Vas-tu l'accepter ?<br><br><click:run_command:%command1%><red><bold>[Accepter le Pacte]",
 			"<dark_gray>Pacte Oublié<br><br><gray>Ce pacte te permet de ne <dark_gray><bold>pas laisser de tête</bold> <gray>derrière toi à ta mort, cependant durant le restant de cette vie tu seras plus faible<br><dark_gray><bold>Vas-tu l'accepter ?<br><br><click:run_command:%command2%>[Accepter le Pacte]"
 		), Map.of("command1", "/kamoofsmp pacte 1", "command2", "/kamoofsmp pacte 2"));
 		ArrayList<BaseComponent[]> result = new ArrayList<>();
